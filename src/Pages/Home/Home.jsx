8524594ba7419477687804efdd5a8d09cc9caa10
AryeshSaha <aryeshsaha5302@gmail.com>
2023-06-17 19:37:21 +0530

    Loading Spinner added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,33 +4,50 @@ import CreateBucket from "../../Components/BucketOps/CreateBucket";
 import Navbar from "../../Components/Navbar/Navbar";
 import { FetchBucketsAction } from "../../Redux/slices/BucketSlice";
 import Buckets from "../Buckets/Buckets";
+import { ScaleLoader } from "react-spinners";
 
 const Home = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(FetchBucketsAction());
-  },[dispatch]);
+  }, [dispatch]);
+
+  const isLoading = useSelector((state) => state.bucket?.isLoading);
 
   const buckets = useSelector((state) => state.bucket?.bucketInfo?.buckets);
 
   return (
     <>
-      <Navbar heading={"Video Player"} />
-      <h1 className="font-semibold m-4 text-2xl">Albums</h1>
-      <div className="container bg-dark rounded-3">
-        <div className="row text-center justify-content-center align-middle">
-          <div className="col-6">
-            <ul>
-              {buckets &&
-                buckets.map((bucket) => {
-                  return <Buckets key={bucket.id} bucket={bucket} />;
-                })}
-            </ul>
-            <CreateBucket />
-          </div>
+      {isLoading ? (
+        <div className="flex justify-center items-center vh-100">
+          <ScaleLoader
+            color={"#1e3a8a"}
+            loading={isLoading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
         </div>
-      </div>
+      ) : (
+        <>
+          <Navbar heading={"Video Player"} />
+          <h1 className="font-semibold m-4 text-2xl">Albums</h1>
+          <div className="container bg-dark rounded-3">
+            <div className="row text-center justify-content-center align-middle">
+              <div className="col-6">
+                <ul>
+                  {buckets &&
+                    buckets.map((bucket) => {
+                      return <Buckets key={bucket.id} bucket={bucket} />;
+                    })}
+                </ul>
+                <CreateBucket />
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
